ac59f774eb4a312214f87a7e0124da7b5910f43c
Dudinskiyvn <Dudinskiyvn@gmail.com>
2024-09-01 19:23:18 +0300

    docs: update index.js for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let subStr = '';
+  for (let i = 0; i < str.length; i++) {
+  if (str[i] == '(' ||  str[i] == '[' || str[i] == '{' || (str[i] == '|' && subStr[subStr.length-1] !== '|' )) { subStr = subStr + str[i]; continue;}
+  if (str[i] == ')' && subStr[subStr.length-1] == '(' ) { subStr = subStr.slice(0, -1); continue;}
+  if (str[i] == ']' && subStr[subStr.length-1] == '[' ) { subStr = subStr.slice(0, -1); continue;}
+  if (str[i] == '}' && subStr[subStr.length-1] == '{' ) { subStr = subStr.slice(0, -1); continue;}
+  if (str[i] == '|' && subStr[subStr.length-1] == '|' ) { subStr = subStr.slice(0, -1); continue;}
+  }  
+  return (subStr == '');
+  }
